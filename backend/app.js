49b7a203f462df324f069a0a207901533ce5cd69
Darcee Quebe <darceerl@byui.edu>
2022-07-14 21:40:53 -0600

    Add basic routes to express app

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,23 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log('first middleware');
-  res.send('Hello from express!');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage');
+});
+
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage');
+});
+
+app.put('/', (req, res) => {
+  res.send('PUT request to the homepage');
+});
+
+app.delete('/', (req, res) => {
+  res.send('DELETE request to the homepage');
 });
 
 module.exports = app;
