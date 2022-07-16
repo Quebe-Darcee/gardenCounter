8da36d441b277fbe08f193b5a0d9ed0f59a062c5
Darcee Quebe <darceerl@byui.edu>
2022-07-16 09:29:06 -0600

    Add comments to server.js file

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+const express = require('express');
 const http = require('http');
 const debug = require('debug')('node-angular');
 var mongoose = require('mongoose');
 const app = require('./backend/app');
 
-const port = process.env.PORT || 3000;
-
-
+// Create mongo database connection
 mongoose.connect('mongodb://localhost:27017/garden',
   { useNewUrlParser: true }, (err, res) => {
     if (err) {
@@ -17,8 +16,12 @@ mongoose.connect('mongodb://localhost:27017/garden',
   }
 );
 
+// Define port and tell express to use it
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
+// Create server
 const server = http.createServer(app);
 
+// Tell server to listen to the defined port
 server.listen(port);
