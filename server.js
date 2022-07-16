b0aef9024ee0c17276774f6191c77e6a1d9cc4cc
Darcee Quebe <darceerl@byui.edu>
2022-07-16 09:10:05 -0600

    Add mongodb database connection

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const http = require('http');
 const debug = require('debug')('node-angular');
+var mongoose = require('mongoose');
 const app = require('./backend/app');
 
 const port = process.env.PORT || 3000;
+
+
+mongoose.connect('mongodb://localhost:27017/garden',
+  { useNewUrlParser: true }, (err, res) => {
+    if (err) {
+      console.log('Connection failed: ' + err);
+    }
+    else {
+      console.log('Connected to database!');
+    }
+  }
+);
 
 app.set('port', port);
 
